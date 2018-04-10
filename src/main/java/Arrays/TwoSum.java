79bef57631c5f2bc8b59f5d836e8c5b9ada40764
JuliaPustovaya <pustovayajulia@gmail.com>
2018-04-10 09:47:21 +0300

    Fixed details

--- src/main/java/Arrays/TwoSum.java
+++ src/main/java/Arrays/TwoSum.java
@@ -5,7 +5,7 @@ public class TwoSum {
 		int[] res = new int[2];
 		for (int i = 0; i < nums.length - 1; i++) {
 			for (int j = i + 1; j < nums.length; j++) {
-				if ((nums[i] + nums[j]) == target) {
+				if (nums[i] + nums[j] == target) {
 					res[0] = i;
 					res[1] = j;
 				}
